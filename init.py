c6c4d7fd87473776781f4431f47897e02a487f6b
KrisGardian <marmanplay9@gmail.com>
2025-05-12 00:37:49 +0200

    Minor changes 	modified:   init.py
    	modified:   init.py
    	deleted:    wav/44100/.indexes
    	deleted:    wav/44100/1x0pv4zhoa4f976697.wav
    	deleted:    wav/44100/4679p0fe71s7rc2v8z.wav
    	deleted:    wav/44100/5d8j6477autb70vi19.wav
    	deleted:    wav/44100/64h0977o0sg1c7q7wz.wav
    	deleted:    wav/44100/690w71l7kcam07qr46.wav
    	deleted:    wav/44100/7k79da0z0b1j4vp767.wav
    	deleted:    wav/44100/bu74h0fc86g9n2y717.wav
    	deleted:    wav/44100/d6107ic0wotr7x6479.wav
    	deleted:    wav/44100/i67wdu9224vm647e1h.wav
    	deleted:    wav/44100/os67107k99hvp446nq.wav
    	deleted:    wav/44100/s294671d72yqu6gf4n.wav
    	deleted:    wav/44100/wz76qh771e58904ktv.wav

--- init.py
+++ init.py
@@ -2,89 +2,56 @@ import audio as audioModule
 import pyqtgraph as pg
 import numpy as np
 
-def clearStatus(status : list):
-    status[0] = 0
-    status[1] = 0
-    status[2] = 0
-    status[3] = 0
-    status[4] = 0
-    status[5] = 0
-    status[6] = 0
-    status[7] = 0
-    status[8] = 0
+
+# init script explanation step by step
+# 1. Create first instance of audio module
+# which lately will provide all required
+# functions & methods.
+# 2. 
+
 
 if __name__ == "__main__":
-    # Status variable for information from the loop.
-    status = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # 0. Riff chunk
-    # 1. Wav chunk 
-    # 2. Data chunk
-    # 3. Audio format
-    # 4. Number of channels
-    # 5. Sample rate
-    # 6. Byte rate
-    # 7. Block align
-    # 8. Bits per sample
 
-    # FileNames list contains all existing file names.
-    fileNames = []
+    # There's just some global vars.
+    global pythonCuda
+    pythonCuda = True
 
-    # Converting all files to raw .wav format(PCM modulation codec).
-    existingRecords = audioModule.getAudioFilesReady(1411, "test-data/wav/44100/", "test-data/miscellaneous/", False)
-
-    # Length of all existing file names.
-    length = len(existingRecords)
+    # Create audioModule class instance.
+    audio = audioModule.audio()
 
     # Counter for main working loop.
     counter = 0
 
-    # Main working loop.
+    while counter != audio.size():
 
-    # While counter != length of all existing file names -> do .
-    while counter != length:
+        # This will get data from files name by name.
+        audio.getInfoFromAudioFile(audio.existingFileNames[counter])
 
-        #First data index.
-        firstDataIndex = 0
-        
-        # Getting info from wav file.
-        firstDataIndex, data = audioModule.getInfoFromAudioFile("test-data/wav/44100/" + str(existingRecords[counter]), status)
+        # Status check.
+        audio.statusCkeck()
 
+        # Audio normalization.
+        audio.normalizeDataFromAudio()
 
-        # Getting more details and determine if everything is good.
-        if status[0] == 0:
-            print("Riff chunk was not founded")
-        if status[1] == 0:
-            print("Wav chunk was not founded")
-        if status[2] == 0:
-           print("Data chunk was not founded")
-        elif status[0] != 0 and status[1] != 0 and status[2] != 0:
-            print("Correction phase:Done\n")
-
-
-        # Audio proccessing phase.
-
-        sample = 150 # status[6] - 1
-
-        data = data[firstDataIndex:sample]
-    
-        data = audioModule.convertAudioFileToStereo(data)
-
+        # Create quick app.
         app = pg.mkQApp()
 
-        window = pg.PlotWidget()
+        # Create main window object.
+        window = pg.PlotWidget(useOpenGL=True)
 
-        pg.setConfigOption("useCupy", "True")
+        # Using gpu to process data.
+        pg.setConfigOption("useCupy", pythonCuda)
 
-        window.useOpenGL(True)
+        # Set plot data.
+        window.plot(np.linspace(0, 10, audio.length()), audio.data, pen='b')
 
-        window.plot(np.linspace(0, 100000, len(data)), data, pen='b')
+        # Set title to graph by file name.
+        window.setTitle(str(audio.existingFileNames[counter]), color='w', size="16pt")
 
-        window.setTitle(str(existingRecords[counter]), color='w', size="16pt")
-
+        # Show window.
         window.show()
 
+        # Execute QT app.
         app.exec()
-
-        clearStatus(status)
 
         counter += 1
